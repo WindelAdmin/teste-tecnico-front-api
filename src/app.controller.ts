8b4cc86cff6b733c7818bd68caa6b928a1ca030c
Wesley Sousa <wesleytuck@outlook.com>
2024-08-13 16:16:30 -0300

    ajuste rotas

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,60 +1,9 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Query,
-} from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
-import { AppService } from './app.service';
-import { RecipeFindManyFilters } from './dtos/find-many-filters.dto';
-import { RecipeCreateDto } from './dtos/recipe-create.dto';
-import { RecipeDeleteInBatchDto } from './dtos/recipe-delete-in-batch.dto';
-import { RecipeModelDto } from './dtos/recipe-model.dto';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('receita')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Post()
-  @ApiOkResponse({ type: RecipeModelDto, isArray: true })
-  async create(@Body() data: RecipeCreateDto): Promise<RecipeModelDto> {
-    return await this.appService.create(data);
-  }
-
-  @Delete(':id')
-  @ApiOkResponse({
-    schema: {
-      example: 'Receita deletada com sucesso.',
-    },
-  })
-  async delete(@Param('id') id: number) {
-    await this.appService.delete(+id);
-    return { message: 'Receita deletada com sucesso.' };
-  }
-
-  @Post('/batch')
-  @ApiOkResponse({
-    schema: {
-      example: 'Receitas deletadas com sucesso.',
-    },
-  })
-  async deleteInBatch(@Body() registers: RecipeDeleteInBatchDto) {
-    await this.appService.deleteInBatch(registers.registersId);
-    return { message: 'Receitas deletadas com sucesso.' };
-  }
-
   @Get()
-  @ApiOkResponse({ type: RecipeModelDto, isArray: true })
-  findMany(@Query() params: RecipeFindManyFilters) {
-    return this.appService.findMany(params);
-  }
-
-  @Get('/:id')
-  @ApiOkResponse({ type: RecipeModelDto })
-  async findOne(@Param('id') id: string) {
-    return this.appService.findOne(+id);
+  async hello() {
+    return 'Olá, seja bem vindo a API de receitas!';
   }
 }
---
+++ src/recipe.controller.ts
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { AppService } from './app.service';
+import { RecipeFindManyFilters } from './dtos/find-many-filters.dto';
+import { RecipeCreateDto } from './dtos/recipe-create.dto';
+import { RecipeDeleteInBatchDto } from './dtos/recipe-delete-in-batch.dto';
+import { RecipeModelDto } from './dtos/recipe-model.dto';
+
+@Controller('recipe')
+export class RecipeController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post()
+  @ApiOkResponse({ type: RecipeModelDto, isArray: true })
+  async create(@Body() data: RecipeCreateDto): Promise<RecipeModelDto> {
+    return await this.appService.create(data);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({
+    schema: {
+      example: 'Receita deletada com sucesso.',
+    },
+  })
+  async delete(@Param('id') id: number) {
+    await this.appService.delete(+id);
+    return { message: 'Receita deletada com sucesso.' };
+  }
+
+  @Post('/batch')
+  @ApiOkResponse({
+    schema: {
+      example: 'Receitas deletadas com sucesso.',
+    },
+  })
+  async deleteInBatch(@Body() registers: RecipeDeleteInBatchDto) {
+    await this.appService.deleteInBatch(registers.registersId);
+    return { message: 'Receitas deletadas com sucesso.' };
+  }
+
+  @Get()
+  @ApiOkResponse({ type: RecipeModelDto, isArray: true })
+  findMany(@Query() params: RecipeFindManyFilters) {
+    return this.appService.findMany(params);
+  }
+
+  @Get('/:id')
+  @ApiOkResponse({ type: RecipeModelDto })
+  async findOne(@Param('id') id: string) {
+    return this.appService.findOne(+id);
+  }
+}
